d2c708c3cbd1c2b35679b60ff20c0dc7b4880efb
tarkue <mrfrostlikchannel@gmail.com>
2026-05-26 18:55:20 +0500

    fix transcript

--- src/entity/material/ui/content/content-viewers/video-viewer.tsx
+++ src/entity/material/ui/content/content-viewers/video-viewer.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Icon } from "@/src/shared/ui/icon";
 import { Typography } from "@/src/shared/ui/typography";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
@@ -6,29 +7,30 @@ import { Suspense } from "react";
 const Player = dynamic(() => import("@/src/shared/ui/player"));
 
 export const MaterialVideoViewer = ({
-  url, transcript
+  url,
+  transcript,
 }: {
   url: string;
   transcript?: string | null;
 }) => {
   return (
     <>
-      <div className="rounded-[12px]">
+      <div className="rounded-xl">
         <Suspense>
           <Player url={url} />
         </Suspense>
-        
       </div>
       {transcript && (
-        <div className="flex flex-col w-full gap-1 p-5 rounded-[14px] bg-base-100 border border-base-200">
-          <Typography.Caption className="text-black opacity-20 font-black! w-full">
-            ТРАНСКРИБАЦИЯ
-          </Typography.Caption>
-          <div className="flex flex-col w-full gap-2">
-            <Typography.Body className="w-full text-wrap">
-              {transcript}
+        <div className="flex flex-col w-full gap-2 p-4 rounded-xl bg-base-raised border border-base-border">
+          <div className="flex gap-3 w-full">
+            <Icon glyph="ai" size="20" color="black" />
+            <Typography.Body bold className="text-black">
+              Краткое содержание
             </Typography.Body>
           </div>
+          <Typography.Body className="w-full text-wrap text-black">
+            {transcript}
+          </Typography.Body>
         </div>
       )}
     </>
